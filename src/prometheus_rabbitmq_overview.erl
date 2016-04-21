dbda6ed8b0fc7e466584ca25025043228e56ca17
Ilya Khaprov <dead.trickster@gmail.com>
2016-04-22 02:12:55 +0300

    added exchange and queue counts

--- src/prometheus_rabbitmq_overview.erl
+++ src/prometheus_rabbitmq_overview.erl
@@ -8,20 +8,28 @@
 -import(rabbit_misc, [pget/2]).
 
 collect_mf(Callback) ->
-    Callback(gauge, rabbitmq_channels, [vhost], "RabbitMQ Channels count"),    
-    Callback(gauge, rabbitmq_connections, [vhost], "RabbitMQ Connections count").
+    Callback(gauge, rabbitmq_connections, [vhost], "RabbitMQ Connections count"),
+    Callback(gauge, rabbitmq_channels, [vhost], "RabbitMQ Channels count"),
+    Callback(gauge, rabbitmq_queues, [vhost], "RabbitMQ Queues count"),
+    Callback(gauge, rabbitmq_exchanges, [vhost], "RabbitMQ Exchanges count").
 
 filter_by_vhost(VHost, Channels) ->
     [I || I <- Channels, pget(vhost, I) =:= VHost].
 
+collect_metrics(rabbitmq_connections, Callback) ->
+    AllConnections = rabbit_mgmt_db:get_all_connections([]),
+    AllVHosts = rabbit_vhost:info_all([name]),
+    [Callback([VHost], length(filter_by_vhost(VHost, AllConnections))) || [{name, VHost}] <- AllVHosts];
 collect_metrics(rabbitmq_channels, Callback) ->
     AllChannels = rabbit_mgmt_db:get_all_channels([]),
     AllVHosts = rabbit_vhost:info_all([name]),
     [Callback([VHost], length(filter_by_vhost(VHost, AllChannels))) || [{name, VHost}] <- AllVHosts];
-collect_metrics(rabbitmq_connections, Callback) ->
-    AllConnections = rabbit_mgmt_db:get_all_connections([]),
+collect_metrics(rabbitmq_queues, Callback) ->
     AllVHosts = rabbit_vhost:info_all([name]),
-    [Callback([VHost], length(filter_by_vhost(VHost, AllConnections))) || [{name, VHost}] <- AllVHosts].
+    [Callback([VHost], length(rabbit_amqqueue:list(VHost))) || [{name, VHost}] <- AllVHosts];
+collect_metrics(rabbitmq_exchanges, Callback) ->
+    AllVHosts = rabbit_vhost:info_all([name]),
+    [Callback([VHost], length(rabbit_exchange:list(VHost))) || [{name, VHost}] <- AllVHosts].
 
 register(Registry) ->
     ets:insert_new(?PROMETHEUS_TABLE, {{Registry, collector, prometheus_rabbitmq_overview, 0}, [], ""}).
