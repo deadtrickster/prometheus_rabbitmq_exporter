cbeccedea149a9a883824cac2ff8c31dbcd26daa
Ilya Khaprov <dead.trickster@gmail.com>
2016-04-22 14:54:20 +0300

    consumer gauge, read channels and connections from ets directly :(

--- src/prometheus_rabbitmq_overview.erl
+++ src/prometheus_rabbitmq_overview.erl
@@ -11,17 +11,18 @@ collect_mf(Callback) ->
     Callback(gauge, rabbitmq_connections, [vhost], "RabbitMQ Connections count"),
     Callback(gauge, rabbitmq_channels, [vhost], "RabbitMQ Channels count"),
     Callback(gauge, rabbitmq_queues, [vhost], "RabbitMQ Queues count"),
-    Callback(gauge, rabbitmq_exchanges, [vhost], "RabbitMQ Exchanges count").
+    Callback(gauge, rabbitmq_exchanges, [vhost], "RabbitMQ Exchanges count"),
+    Callback(gauge, rabbitmq_consumers, [], "RabbitMQ Consumers count").
 
 filter_by_vhost(VHost, Channels) ->
     [I || I <- Channels, pget(vhost, I) =:= VHost].
 
 collect_metrics(rabbitmq_connections, Callback) ->
-    AllConnections = rabbit_mgmt_db:get_all_connections([]),
+    AllConnections = created_events(connection_stats),
     AllVHosts = rabbit_vhost:info_all([name]),
     [Callback([VHost], length(filter_by_vhost(VHost, AllConnections))) || [{name, VHost}] <- AllVHosts];
 collect_metrics(rabbitmq_channels, Callback) ->
-    AllChannels = rabbit_mgmt_db:get_all_channels([]),
+    AllChannels = created_events(channel_stats),
     AllVHosts = rabbit_vhost:info_all([name]),
     [Callback([VHost], length(filter_by_vhost(VHost, AllChannels))) || [{name, VHost}] <- AllVHosts];
 collect_metrics(rabbitmq_queues, Callback) ->
@@ -29,7 +30,16 @@ collect_metrics(rabbitmq_queues, Callback) ->
     [Callback([VHost], length(rabbit_amqqueue:list(VHost))) || [{name, VHost}] <- AllVHosts];
 collect_metrics(rabbitmq_exchanges, Callback) ->
     AllVHosts = rabbit_vhost:info_all([name]),
-    [Callback([VHost], length(rabbit_exchange:list(VHost))) || [{name, VHost}] <- AllVHosts].
+    [Callback([VHost], length(rabbit_exchange:list(VHost))) || [{name, VHost}] <- AllVHosts];
+collect_metrics(rabbitmq_consumers, Callback) ->
+    Callback([], ets:info(consumers_by_queue, size)).
+
 
 register(Registry) ->
     ets:insert_new(?PROMETHEUS_TABLE, {{Registry, collector, prometheus_rabbitmq_overview, 0}, [], ""}).
+
+
+
+created_events(Type) ->
+    ets:select(Type, [{{{'_', '$1'}, '$2', '_'}, [{'==', 'create', '$1'}],
+                       ['$2']}]).
