6fdd7d57c3857598e520f474bec818f8fffd498b
Ilya Khaprov <dead.trickster@gmail.com>
2016-08-12 18:02:35 +0300

    support messages_stat config

--- src/prometheus_rabbitmq_exporter_config.erl
+++ src/prometheus_rabbitmq_exporter_config.erl
@@ -1,12 +1,22 @@
 -module(prometheus_rabbitmq_exporter_config).
 
 -export([path/0,
-         format/0]).
+         format/0,
+         queue_messages_stat/0]).
 
 -define(DEFAULT_PATH, "metrics").
 -define(DEFAULT_FORMAT, prometheus_text_format).
+-define(DEFAULT_QUEUE_MESSAGES_STAT, [messages_published_total,
+                                      messages_confirmed_total,
+                                      messages_delivered_total,
+                                      messages_delivered_no_ack_total,
+                                      messages_get_total,
+                                      messages_get_no_ack_total,
+                                      messages_redelivered_total,
+                                      messages_returned_total]).
 -define(DEFAULT_CONFIG, [{path, ?DEFAULT_PATH},
-                         {format, ?DEFAULT_FORMAT}]).
+                         {format, ?DEFAULT_FORMAT},
+                         {queue_messages_stat, ?DEFAULT_QUEUE_MESSAGES_STAT}]).
 
 config() ->
   application:get_env(prometheus, rabbitmq_exporter, ?DEFAULT_CONFIG).
@@ -18,3 +28,7 @@ path() ->
 format() ->
   Config = config(),
   proplists:get_value(format, Config, ?DEFAULT_FORMAT).
+
+queue_messages_stat() ->
+  Config = config(),
+  proplists:get_value(queue_messages_stat, Config, ?DEFAULT_QUEUE_MESSAGES_STAT).
